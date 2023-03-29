147c9d7904f316e6e2405f06177f80d41d4e0b4f
ruaridhg <ruaridhg@users.noreply.github.com>
2023-03-29 16:57:15 +0100

    Added a different version of install randomiser to work on rg computer

---
+++ install_randomiser_rg.sh
@@ -0,0 +1,6 @@
+source ~/.bash_profile
+#!/usr/bin/env bash
+# zip randomiser, launch blender and install+enable
+cd blender_randomiser
+zip -r randomiser.zip randomiser/
+blender ../sample.blend --python ./install_and_enable_addons.py -- ./randomiser.zip
