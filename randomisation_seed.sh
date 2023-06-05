c3cd9370dc44bf5aee98f0234f4e9c8e334bd44a
ruaridhg <ruaridhg@users.noreply.github.com>
2023-06-05 16:31:48 +0100

    Added FS new zip command to rand_seed.sh

--- randomisation_seed.sh
+++ randomisation_seed.sh
@@ -2,5 +2,5 @@
 source ~/.bash_profile
 
 # zip randomiser, launch blender and install+enable
-zip -r randomiser.zip randomiser/
+zip randomiser.zip -FS -r randomiser/
 blender sample.blend --python install_and_enable_addons.py -- ./randomiser.zip --seed 32
