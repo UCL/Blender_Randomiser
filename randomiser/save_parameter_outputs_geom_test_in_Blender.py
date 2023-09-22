d935c490c6fc19ae58388f7f1bf1657b364c3d89
ruaridhg <ruaridhg@users.noreply.github.com>
2023-09-22 14:23:56 +0100

    Add modified draw_sockets_list func to test_in_Blender file for geom_mat

--- randomiser/save_parameter_outputs_geom_test_in_Blender.py
+++ randomiser/save_parameter_outputs_geom_test_in_Blender.py
@@ -75,10 +75,103 @@ print(data)
 
 
 #### SUBPANEL materials
+
+
+def get_material_nodes_to_randomise_indep(
+    material_str: str = "Material",
+    node2randomise_prefix: str = "random",
+):
+    """Get list of *independent* input nodes to randomise for a given material.
+
+    Input nodes are defined as nodes with no input sockets.
+    The input nodes to randomise are identified because their
+    name is prefixed with node2randomise_prefix (case insensitive).
+
+    Both independent nodes, and nodes inside a group are searched.
+    The 'artificial' nodes that show up inside a node group, usually named
+    'Group input' or 'Group output' are excluded from the search.
+
+    Parameters
+    ----------
+    material_str : str, optional
+        name of the material, by default "Material"
+    node2randomise_prefix : str, optional
+        prefix that identifies the nodes to randomise, by default 'random'
+
+    Returns
+    -------
+    list
+        list of all *group* input nodes to randomise for this material
+    """
+
+    # list of nodes for current material
+    # not belonging to a group
+    list_material_nodes_indep = []
+    for nd in bpy.data.materials[material_str].node_tree.nodes:
+        if nd.type != "GROUP":
+            list_material_nodes_indep.append(nd)
+
+    # find input nodes that startwith random
+    list_input_nodes = get_nodes_to_randomise_from_list(
+        list_material_nodes_indep
+    )
+
+    return list_input_nodes
+
+
+def get_nodes_to_randomise_from_list(
+    list_candidate_nodes: list,
+    node2randomise_prefix: str = "random",
+):
+    """Get list of nodes to randomise from list.
+
+    Input nodes are defined as nodes with no input sockets.
+    The nodes to randomise are input nodes whose name starts
+    with 'node2randomise_prefix' (case insensitive).
+
+    The 'artificial' nodes that show up inside a node group, usually named
+    'Group input' or 'Group output' are excluded from the search.
+
+    Parameters
+    ----------
+    list_candidate_nodes : list
+        list of the candidate nodes to randomise
+    node2randomise_prefix : str, optional
+        prefix that identifies the nodes to randomise, by default 'random'
+
+    Returns
+    -------
+    list
+        list of the input nodes to randomise
+    """
+
+    # ensure list_candidate_nodes is unique
+    list_candidate_nodes = list(set(list_candidate_nodes))
+
+    # find input nodes that start with the random keyword
+    # excluding 'Group' artificial nodes
+    list_input_nodes = [
+        nd
+        for nd in list_candidate_nodes
+        if len(nd.inputs) == 0
+        and nd.name.lower().startswith(node2randomise_prefix.lower())
+        and nd.type
+        not in [
+            "GROUP_INPUT",
+            "GROUP_OUTPUT",
+        ]
+    ]
+
+    return list_input_nodes
+
+
 # Get list of input nodes to randomise
 # for this subpanel's material
-# cs = bpy.context.scene
-# subpanel_material = cs.socket_props_per_material.collection[0]
+cs = bpy.context.scene
+subpanel_material = cs.socket_props_per_material.collection[0]
+list_input_nodes = get_material_nodes_to_randomise_indep(
+    subpanel_material.name
+)
 # list_input_nodes = nodes2rand.get_material_nodes_to_randomise_indep(
 #     subpanel_material.name
 # )
