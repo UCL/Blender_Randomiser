42e4cdc0d1826c7b030f6d3738f6e76d42eddbca
ruaridhg <ruaridhg@users.noreply.github.com>
2023-10-16 16:23:35 +0100

    Changed back to sample.blend file

--- install_randomiser.sh
+++ install_randomiser.sh
@@ -4,4 +4,4 @@ source ~/.bash_profile
 
 # zip randomiser, launch blender and install+enable
 zip randomiser.zip -FS -r randomiser/
-blender define_prop.blend --python install_and_enable_addons.py -- ./randomiser.zip
+blender sample.blend --python install_and_enable_addons.py -- ./randomiser.zip
